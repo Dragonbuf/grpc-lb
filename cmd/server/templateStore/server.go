71e8d46c2d4543d0fb9ae4b9a4b568938913537c
zhangyanhe <zhangyanhe@maka.im>
2019-02-21 12:22:30 +0800

    update proto 2 => 3

--- cmd/server/templateStore/server.go
+++ cmd/server/templateStore/server.go
@@ -4,24 +4,24 @@ import (
 	"context"
 	"errors"
 	"github.com/wwcd/grpc-lb/cmd/basegrpc"
-	model2 "github.com/wwcd/grpc-lb/cmd/server/templateStore/model"
-	ts "github.com/wwcd/grpc-lb/cmd/server/templateStore/proto"
 	"google.golang.org/grpc"
+	model2 "grpc-lb/cmd/server/templateStore/model"
+	ts "grpc-lb/cmd/server/templateStore/proto"
 )
 
 func main() {
-	var base basegrpc.BaseGrpc
+	var base = basegrpc.BaseGrpc{ServiceName: "templateStoreService"}
 	lis := base.NewBaseGrpc()
 
 	s := grpc.NewServer()
-	ts.RegisterTemplateStoreServer(s,&server{})
+	ts.RegisterTemplateStoreServer(s, &server{})
 	_ = s.Serve(lis)
 }
 
-
 type server struct {
 }
-func (t *server) Get(ctx context.Context, in *ts.ShowRequest)(*ts.ShowReply, error)  {
+
+func (t *server) Get(ctx context.Context, in *ts.ShowRequest) (*ts.ShowReply, error) {
 
 	if in.GetTemplateId() == "" {
 		return nil, errors.New("templateIdEmpty")
@@ -33,17 +33,16 @@ func (t *server) Get(ctx context.Context, in *ts.ShowRequest)(*ts.ShowReply, err
 		return nil, err
 	}
 
-
 	return &ts.ShowReply{
-		TemplateId:model.TemplateId,
-		DesignerId:model.DesignerId,
-		Price:model.Price,
-		Version:model.Version,
-		Star:model.Star,
-		Level:model.Level,
-		Sort:model.Sort,
-		IsVipFree:model.IsVipFree,
-		TemplateInfo:model.TemplateInfo,
-		TemplateProperty:model.TemplateProperty,
+		TemplateId:       model.TemplateId,
+		DesignerId:       model.DesignerId,
+		Price:            model.Price,
+		Version:          model.Version,
+		Star:             model.Star,
+		Level:            model.Level,
+		Sort:             model.Sort,
+		IsVipFree:        model.IsVipFree,
+		TemplateInfo:     model.TemplateInfo,
+		TemplateProperty: model.TemplateProperty,
 	}, nil
 }
